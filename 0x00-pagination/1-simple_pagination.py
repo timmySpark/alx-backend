0c09c9210e22c13dbeffce08e37f1facde14b13f
timmySpark <timmyspark1@gmail.com>
2024-09-02 08:25:55 +0000

    [feat] :- working on task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,8 +35,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Retrieves a page of data."""
-        assert page > 0 and page_size > 0
-        assert isinstance(page, int) and isinstance(page_size, int)
+        assert isinstance(page, int) and page >= 1
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
         try:
